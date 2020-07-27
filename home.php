c905d11baf3829286f438ad3cc0249627ac00fc5
johnab2004 <51392431+johnab2004@users.noreply.github.com>
2020-07-27 17:30:00 +1200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <title> WGC Canteen | Home </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="section">
+    <div class="container1">
+        <a href="home.html"><img class="logo" src="wgclogo.png" alt="WGC logo" width="200" height="200"></a>
+    </div>
+</div>
+
+<div class="section">
+    <div class="container1">
+        <a href="index.html"></a>
+    </div>
+</div>
+<div class="section">
+    <div class="container2">
+        <h1>WGC canteen</h1>
+        <p class="hug">Everyone welcome :)</p>
+    </div>
+</div>
+
+
+<nav class="clearfix">
+    <a href="contactus.html">Contact us </a>
+    <a href="menu.html">Specials </a>
+    <a href="aboutus.html">Drinks </a>
+    <a href="index.html">Food </a>
+</nav>
+<div>
+
+    <div class="section divider-home"></div>
+    <div class="section">
+        <div class="container">
+
+        </div>
+    </div>
+</div>
+
+<div class="section divider-1"></div>
+<div class="section clearfix">
+    <div class="container">
+
+        <div class="coltxt">
+            <h2>Food</h2>
+            <p> Abia's cafe is a cafe that was formed all the way back in 1960's. We serve freshly brewed coffee and many other drinks and serve a delicious variety of food. Our staff are helpful and always happy to help out. The cafe was passed down from generations of Abia's family.
+                We are a great place to come and have a great time with family and friends. We have many great items for you to eat as well as buy and bring home for a special treat on another day. We also do catering on different events such as partys, weddings and many more.<br><br>
+        </div>
+    </div>
+</div>
+
+<div class="section divider-2"></div>
+<div class="section clearfix">
+    <div class="container">
+
+        <div class="coltxt">
+            <h2>Drinks</h2>
+
+
+            <p> Our menu can be eaten any time, breafast, lunch or dinner. The menu can be altered to you liking! The breakfast menu includes freshly brewed coffee, bacon and eggs and many more. The lunch menu includes sandwiches, pies and many more. The dinner menu includes fish and chips, beef wellington and many more. We also sell our own little goodies like slices, brownies and cakes! we also have discounts on items on special days come in to see how much you can gte off your next order.The image seen above is our own recipe pavlova (available for sale in store) come in and try it now!</p>
+        </div>
+    </div>
+
+
+    <div class="section divider-3"></div>
+    <div class="section clearfix">
+        <div class="container">
+
+
+            <div class="coltxt">
+                <h2>Contact us</h2>
+
+                <p> Unfortunatly since we are a growing business we have only one shop, that is located on the water front If you want to contact us you can call us on the number 021 936 5096. We have some beautiful views from our balcony (which is available with tables)If you want to send us an email you can contact us on abiascafe@gmail.com. If you have any quierys or questions you can ask them on the form in the contact page. <br><br></p>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
